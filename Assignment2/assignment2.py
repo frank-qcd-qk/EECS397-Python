ce60daef19dcc49891043c79a509767ca93c9dbf
frank-qcd-qk <cxq41@case.edu>
2019-09-19 13:29:41 -0400

    formatted documentation

--- Assignment2/assignment2.py
+++ Assignment2/assignment2.py
@@ -28,7 +28,8 @@ MINIMAL = True
 3. Print the number of valid lines followed by the number of invalid lines to stdout. The values should be separated by a single space and there should be no trailing whitespace.
 """
 
-def customPrint(input_str,level=0):
+
+def customPrint(input_str, level=0):
     message = str(input_str)
     if level == 0:
         if DEBUG:
@@ -47,72 +48,74 @@ def customPrint(input_str,level=0):
     else:
         print(message)
 
+
 #! Input parameter parser
 try:
     SCHEMA_FILE_NAME = sys.argv[1]
     DATA_FILE_NAME = sys.argv[2]
 except IndexError:
     sys.stdout.write("0 0")
-    customPrint("No input arguments specified. Please use 'python3 assignment2.py SCHEMA_FILE_ADDRESS DATA_FILE_ADDRESS'",5)
+    customPrint(
+        "No input arguments specified. Please use 'python3 assignment2.py SCHEMA_FILE_ADDRESS DATA_FILE_ADDRESS'", 5)
     sys.exit(99)
 except:
     sys.stdout.write("0 0")
-    customPrint("Some other issue observed. Please check...",5)
+    customPrint("Some other issue observed. Please check...", 5)
     sys.exit(99)
 else:
     customPrint("schema file name: "+SCHEMA_FILE_NAME)
     customPrint("data file name: "+DATA_FILE_NAME)
 finally:
-    customPrint("File name intake completed successfully",1)
+    customPrint("File name intake completed successfully", 1)
 
 #! File validity checker:
 try:
     #! Schema Processing
     with open(SCHEMA_FILE_NAME, 'r') as schema:
         for line in schema:
-            #? Judge if the line is comment or not
+            # ? Judge if the line is comment or not
             if line.startswith("#"):
                 line_count = line_count+1
                 customPrint("Line"+str(line_count)+"Comment Line/r/n")
                 continue
             else:
-                #? Line not comment, search for width
+                # ? Line not comment, search for width
                 options = line.split()
 
                 line_count = line_count+1
                 customPrint("Line"+str(line_count))
                 customPrint(str(options))
-                
-                #? Search for the keyword in the line
+
+                # ? Search for the keyword in the line
                 try:
                     index = options.index("width")
                 except ValueError:
-                    customPrint("Line does not contain keywords!",3)
+                    customPrint("Line does not contain keywords!", 3)
                     continue
                 else:
                     customPrint("Width keyword found! AT:"+str(index))
-                
-                #? Read the index value and summarize them
+
+                # ? Read the index value and summarize them
                 try:
                     WIDTH_INTEGER = int(options[index+1])
                 except ValueError:
-                    customPrint("Things after Width is not a value!!!",4)
+                    customPrint("Things after Width is not a value!!!", 4)
                     continue
                 else:
-                    LINE_WIDTH  = WIDTH_INTEGER + LINE_WIDTH
-    customPrint("Schema File Shows LINE_WIDTH = "+str(LINE_WIDTH),1)
+                    LINE_WIDTH = WIDTH_INTEGER + LINE_WIDTH
+    customPrint("Schema File Shows LINE_WIDTH = "+str(LINE_WIDTH), 1)
 
     #! Data Processing
-    with open(DATA_FILE_NAME,'r') as dataFile:
+    with open(DATA_FILE_NAME, 'r') as dataFile:
         for line in dataFile:
-            customPrint("Line Length is: "+str(len(line)),1)
-            if(len(line)==(LINE_WIDTH+1)):
+            customPrint("Line Length is: "+str(len(line)), 1)
+            if(len(line) == (LINE_WIDTH+1)):
                 VALID_LINE = VALID_LINE+1
             else:
                 INVALID_LINE = INVALID_LINE+1
 except FileNotFoundError:
-    customPrint("ERROR! File is not found!",5)
+    customPrint("ERROR! File is not found!", 5)
 else:
     sys.stdout.write(""+str(VALID_LINE)+" "+str(INVALID_LINE))
 finally:
-    customPrint("Program Complete",1)
+    customPrint("Program Complete", 1)
